8f7cc751ec095457a5131b7e0a16a842136a9afc
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-07-28 06:36:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = 3000
 app.set("trust proxy", 1);
 app.use(json())
 app.use(cookieParser())
-app.use(cors({origin:"https://kizerr.pages.dev",credentials:true ,exposedHeaders: \["Set-cookie"\]}))
+app.use(cors({origin:"https://kizerr.pages.dev",credentials:true ,exposedHeaders: ["Set-Cookie"]}))
 app.use('/', userRouter)
 app.use('/', authRouter)
 app.use('/', gigRouter)
