b50581c61ec9a4c0de925b741b4a3b5aca671504
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-08-07 03:13:34 +0300

    new

--- models/user.model.js
+++ models/user.model.js
@@ -5,12 +5,10 @@ const userSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true,
   },
   password: {
     type: String,
