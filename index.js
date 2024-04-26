09a0b66e1a02b98620a906434e09c95743f2ca66
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2024-04-26 15:37:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,12 +22,6 @@ const corsOptions ={
 }
 
 app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://kizerr.pages.dev');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   next();
-// });
 app.use('/', userRouter)
 app.use('/', authRouter) 
 app.use('/', gigRouter)
